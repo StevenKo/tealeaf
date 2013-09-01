bb4c440895bfe5add6c88dc415a645d1d6b6ce84
Steven Ko <chunyuko85@gmaill.com>
2013-09-01 22:11:51 +0800

    extract module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  include Voteable
+  
   belongs_to :creator, foreign_key: :user_id, class_name: "User" 
   has_many :comments
 
@@ -9,10 +11,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
 
   after_validation :generate_slug
-
-  def total_votes
-    self.votes.where(vote: true).size - self.votes.where(vote: false).size
-  end
 
   def generate_slug
     self.slug = self.title.gsub(" ","-").downcase
